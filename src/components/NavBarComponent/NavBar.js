78fe2626ec75be1e7707a824747e008ccb2710cd
Lucas Szymkow <lucasszymkow01@gmail.com>
2023-02-02 19:50:38 -0300

    Improved Navbar

--- src/components/NavBarComponent/NavBar.js
+++ src/components/NavBarComponent/NavBar.js
@@ -1,14 +1,22 @@
 import CartWidget from "../CartWidgetComponent/CartWidget";
 import "./NavBar.css";
+import searchLogo from "./assets/searchIcon.svg";
 
 export default function NavBar() {
   return (
     <nav className="navBar">
       <div className="navBarContainer">
-        <ul className="navBarList">
-          <li className="mainLogo">FastMarket</li>
+        <div className="navBarList">
+          <div className="mainLogo">FastMarket</div>
+          <div className="searchBarContainer">
+            <input
+              className="searchBarInput"
+              placeholder="Buscar productos..."
+            ></input>
+            <img src={searchLogo} alt="Buscar" className="searchLogo" />
+          </div>
           <CartWidget />
-        </ul>
+        </div>
       </div>
     </nav>
   );
